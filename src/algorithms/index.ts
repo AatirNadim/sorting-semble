f2b600860dc00bc71c34900ca9c4feae4d2a38e1
AatirNadim <aatir.nadim@gmail.com>
2023-09-03 19:19:30 +0530

    added the functions

---
+++ src/algorithms/index.ts
@@ -0,0 +1,19 @@
+import bubbleSort from "./bubbleSort";
+import shellSort from "./shellSort";
+import quickSort from "./quickSort";
+import heapSort from "./heapSort";
+import mergeSort from "./mergeSort";
+import selectionSort from "./selectionSort";
+import emptySort from './emptySort'
+
+
+export {
+  bubbleSort,
+  shellSort,
+  emptySort,
+  quickSort,
+  heapSort,
+  mergeSort,
+  selectionSort
+}
+
